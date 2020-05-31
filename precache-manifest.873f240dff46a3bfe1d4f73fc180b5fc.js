e4979b763dce3186303bb9a7c259b1dcd1322e0c
meekkaran <meekkaran2000@gmail.com>
2020-05-31 14:23:25 +0300

    Updates

---
+++ precache-manifest.873f240dff46a3bfe1d4f73fc180b5fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7890e9d035b1cfc5b0a6fb675fe0a16a",
+    "url": "/ReactJS/index.html"
+  },
+  {
+    "revision": "aee0d724136aec6b135a",
+    "url": "/ReactJS/static/css/main.6d41c84f.chunk.css"
+  },
+  {
+    "revision": "d38f5d1ce188b9a72145",
+    "url": "/ReactJS/static/js/2.915285b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS/static/js/2.915285b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aee0d724136aec6b135a",
+    "url": "/ReactJS/static/js/main.a828f545.chunk.js"
+  },
+  {
+    "revision": "b895741712db7a1f6282",
+    "url": "/ReactJS/static/js/runtime-main.320edf14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactJS/static/media/logo.5d5d9eef.svg"
+  }
+]);
